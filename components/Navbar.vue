4dba56e93c4df34f0217d2647ee9283cdb42c7c8
GitPAV <pierre.vincs@gmail.com>
2021-04-14 19:02:42 +0200

    Setting up, API call, architecture

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar">
+    <p>Je suis une navbar</p>
+  </div>
+</template>
+
+<style>
+.navbar {
+  width: 100%;
+  height: 2em;
+  background-color: grey;
+  position: fixed;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,19 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        pokedex
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="container flex-center">
+    <Navbar />
+    <div id="List-container">
+      <h1 class="title">Pokédex</h1>
+
+      <p v-if="$fetchState.pending">Fetching pokemons...</p>
+      <p v-else-if="$fetchState.error">The pokemon run away !(An error occurred) :(</p>
+      <div class="pokemon-list flex-center" v-else>
+          <div class="pokemon-item flex-center" v-for="(post, index) of pokemonList.results" :key="post.name">
+            <p>{{ pokemonList.length }}</p>
+            <img :src="require('assets/official-artwork/' + (index + 1) + '.png')" alt="">
+            <p>{{ post.name }}</p>
+          </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -30,46 +21,68 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      pokemonList: Array,
+    }
+  },
+  methods: {
+  },
+
+  // Pokemon api call
+  async fetch() {
+    this.pokemonList = await fetch(
+      'https://pokeapi.co/api/v2/pokemon?limit=151'
+    ).then(res => res.json())
+    console.log(this.pokemonList)
+  }
+
+})
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+/* Utilities class */
+.flex-center {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+/* Regular class & id, sorted by top to bottom */
+.container {
+  flex-direction: column;
   text-align: center;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+#List-container {
+  margin-top: 100px;
+  width: 100%;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.pokemon-list {
+  width: 95%;
+  margin-left: auto;
+  margin-right: auto;
+  flex-wrap: wrap;
 }
 
-.links {
-  padding-top: 15px;
+.pokemon-item {
+  width: 30%;
+  flex-direction: column;
+  margin: 15px 0 15px 0;
 }
+
+.pokemon-item img {
+  width: 75%;
+}
+
+.pokemon-item p {
+  font-size: 1.6em;
+  font-style: italic;
+}
+
+/* .pokemon-item img:hover {
+  transform: scale(.95);
+} */
 </style>
